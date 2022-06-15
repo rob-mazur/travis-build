31151ef302d03085a6d2de5837433a0aa317bb14
Konrad Wrzos <konrad@travis-ci.org>
2022-06-15 15:06:13 +0200

    Testing bellsoft

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -20,12 +20,12 @@ travis_install_jdk_package_adopt() {
   local JAVA_VERSION
   JAVA_VERSION="$1"
   sudo apt-get update -yqq
-  PACKAGE="adoptopenjdk-${JAVA_VERSION}-hotspot"
-  wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-  sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+  PACKAGE="bellsoft-java${JAVA_VERSION}"
+  wget -qO - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
+  sudo add-apt-repository  'deb [arch='{$TRAVIS_CPU_ARCH}'] https://apt.bell-sw.com/ stable main'
   sudo apt-get update -yqq
   sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
-  travis_cmd "export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-${JAVA_VERSION}-hotspot-${TRAVIS_CPU_ARCH}" --echo
+  travis_cmd "export JAVA_HOME=/usr/lib/jvm/bellsoft-java${JAVA_VERSION}-${TRAVIS_CPU_ARCH}" --echo
   travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
   sudo update-java-alternatives -s "$PACKAGE"*
 }
